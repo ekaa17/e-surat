fcb31bb5ac1a817f68c3d1f64c4bcd02843c0e93
ndhpratw <bolabolabangkalan5@gmail.com>
2024-10-18 09:57:23 +0700

    [FEAT] frontend surat penawaran harga

---
+++ resources/views/pages/surat/surat_penawaran_harga.blade.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Surat Penawaran Harga</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="{{ asset('assets/img/logo.png') }}" rel="icon">
+  <link href="{{ asset('assets/img/logo.png') }}" rel="logo">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+
+  <div>
+    <table width=100%>
+      <tr>
+        <td width="20%" class="text-center">
+          <img src="./LogoIDKY.png" alt="Logo IndoKarya" width="175px">
+        </td>
+        <td width="80%" class="my-0 py-0">
+          <h6 class="text-primary">PT. INDOKARYA JASA PRIMA</h6>
+          <p style="font-size: 12px; text-align: justify;" class="my-0 py-0">
+            DISTRIBUTOR OF VALCE, PIPES & FITTING, ELECTRICAL, PNEUMTAUC, CONTRAKTOR & SAFETY EQUIPMENT (TRADING & SUPPLIER)
+          </p>
+          <p style="font-size: 10px" class="my-0 py-0">
+            Office : jln. raya cilegon (Pondok Cilegon Indah) Blok B.21 No.10 Kec.Cibeber kode pos 42423 tlp. 081903811118
+          </p>
+        </td>
+      </tr>
+    </table>
+
+    <hr style="border: 2px solid black">
+
+    <div class="d-flex text-align-center justify-content-between">
+        <div>
+            <b style="font-size: 10px">
+                To : Pak Romaki <br>
+                PT. HUTAMAKARYA
+            </b>
+        </div>
+        <div class="m-0 p-0">
+            <b style="font-size: 10px">
+                PT. INDOKARYA JASA PRIMA <br>
+                JL. RAYA PONDOK CILEGON INDAH <br>
+                (BLOK B.21 NO.10) CILEGON - BANTEN <br>
+                TELP : 081803811118 <br>
+                FAX : 087809374793 <br>
+                ATT : JO MIFTAH <br><br>
+                QTC :  001/IJP-HTMK/III/2024
+            </b>
+        </div>
+    </div>
+
+    <div style="margin-top: 35px; display: flex; justify-content:center; align-items:center;">
+      <table class="table" style="font-size: 10px">
+        <thead>
+          <th>NO</th>
+          <th>NAMA BARANG</th>
+          <th>QTY</th>
+          <th>UNIT</th>
+          <th>PRICE</th>
+          <th>TOTAL</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Barang A</td>
+                <td>4</td>
+                <td>PC</td>
+                <td>150000</td>
+                <td>25000</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Barang A</td>
+                <td>4</td>
+                <td>PC</td>
+                <td>150000</td>
+                <td>25000</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Barang A</td>
+                <td>4</td>
+                <td>PC</td>
+                <td>150000</td>
+                <td>25000</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Barang A</td>
+                <td>4</td>
+                <td>PC</td>
+                <td>150000</td>
+                <td>25000</td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>Barang A</td>
+                <td>4</td>
+                <td>PC</td>
+                <td>150000</td>
+                <td>25000</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>Total</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>25000000</td>
+            </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <b style="font-size: 10px">
+        SYARAT & KETENTUAN <br>
+        <u>
+            PENGIRIMAN & PEMBAYARAN : 3 HARI KERJA SETELAH KONFIRMASI ORDER & DP CASH 50%
+        </u> <br>
+                                    PELUNASAN CASH SEBELUM BARANG KAMI KONFIRMASI <br>
+        VALIDITY : 1 MINGGU SETELAH TANGGAL PENAWARAN <br>
+        HARGA BELUM TERMASUK TAX 11% <br> <br>
+        PEMBAYARAN :   1630007137998 ( INDOKARYA JASA PRIMA )
+    </b>
+
+    <br>
+    
+    <table style="font-size: 10px">
+      <tr>
+        <td>
+          CILEGON, 18 MARET 2024
+          <br><br><br>
+          BEST REGARDS
+          <br><br>
+          ttd
+          <br><br>
+          <p>
+              <u>PT. INDOKARYA JASA PRIMA</u> <br>
+              JO MIFTAH (082112345678) <br>
+              Email : ptindokaryajasaprima@Gmail.com
+          </p>
+
+          <b> ______________ </b>
+        </td>
+      </tr>
+    </table>
+  </div>
+
+  <script type="text/javascript">
+    window.print();
+  </script>
+</body>
+
+</html>
