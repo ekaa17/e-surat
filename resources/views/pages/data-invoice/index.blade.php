03791d2f9e85956031d23c84846acaa332265410
ekaa17 <ekasalafi@gmail.com@gmail.com>
2024-11-08 04:43:00 +0700

    coba2

--- resources/views/pages/data-invoice/index.blade.php
+++ resources/views/pages/data-invoice/index.blade.php
@@ -6,7 +6,7 @@
         <nav>
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="/">Home</a></li>
-                <li class="breadcrumb-item active">Data Invoice Belum Jadi</li>
+                <li class="breadcrumb-item active">Data Invoice </li>
             </ol>
         </nav>
     </div><!-- End Page Title -->
